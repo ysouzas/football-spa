926f4dad936ac4c16bf919a88d49e96032ad5445
Yago Santos <yago.santos@quartalfs.com>
2023-04-02 12:56:36 +0100

    update to new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,74 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-
-import { Player } from './models/player';
-import { PlayerService } from './services/player.service';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  title = "Pelada Baixo Nivel";
-  players: Player[] = [];
-  idsToSort: string[] = [];
-  times: string = "";
-  hidden = true;
-  constructor(private playerService: PlayerService) {}
-
-  ngOnInit(): void {
-    this.getPlayers();
-  }
-
-  getPlayers() {
-    this.playerService.getPlayers().subscribe((response: any) => {
-      console.log(response);
-      this.players = response;
-    });
-  }
-
-  onSelectPlayer(id: string) {
-    const hasId = this.idsToSort.includes(id);
-
-    if (hasId) {
-      const index = this.idsToSort.indexOf(id, 0);
-      if (index > -1) {
-        this.idsToSort.splice(index, 1);
-      }
-    } else {
-      this.idsToSort.push(id);
-    }
-  }
-
-  getTeams() {
-    this.playerService.getTeams(this.idsToSort).subscribe((asd) => {
-      var aaa = "";
-      asd.forEach((element: any, index: any) => {
-        aaa += `Time ${index + 1} - Score: ${element.score.toFixed(2)}\n`;
-        element.teams.forEach((a: any) => {
-          aaa += `${a.name} - ${a.generalScore.toFixed(2)}\n`;
-        });
-        aaa += `-------------------------------------\n`;
-      });
-      this.times = aaa;
-      this.hidden = false;
-    });
-  }
-
-  ranking() {
-    this.playerService.ranking(this.idsToSort).subscribe((asd) => {
-      var date = new Date(asd.date);
-      var aaa = `Ranking ${date.toLocaleDateString(
-        "pt-br"
-      )} - ${new Date().toLocaleDateString("pt-br")} \n`;
-      console.log(asd);
-      asd.players.forEach((element: any, index: any) => {
-        aaa += `${index + 1} - ${element.name}: ${element.generalScore.toFixed(
-          2
-        )}\n`;
-      });
-      this.times = aaa;
-      this.hidden = false;
-    });
-  }
+export class AppComponent {
+  title = 'football-spa';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,18 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    HttpClientModule,
-    SharedModule,
-    MatGridListModule,
-    MatButtonModule,
-    MatIconModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
