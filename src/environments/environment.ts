b52d32378dae7846a4fdf8a1ead3b96acfbd53d3
Yago Santos <>
2023-04-09 22:16:54 +0100

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7278/api',
-  apiURLPlayers:
-    'https://peladabaixonivelfunctions.azurewebsites.net/api/football_functions',
+  apiUrl: '',
+  apiURLPlayers: '',
 };
