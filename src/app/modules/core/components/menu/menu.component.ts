46a01d55bc8d70ba24b44e16618f15a35443b64e
Yago Santos <>
2023-04-09 23:08:24 +0100

    apelacao

--- src/app/modules/core/components/menu/menu.component.ts
+++ src/app/modules/core/components/menu/menu.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent {}
+export class MenuComponent {
+  url = environment.apiURLPlayers;
+}
